b44f92cba4998fa1276be1dd0efc366633223039
kadekekada <81542416+kadekekada@users.noreply.github.com>
2021-04-27 20:54:49 +0800

    Initial commit

---
+++ app/src/main/java/com/deka/sate/data/Sate.java
@@ -0,0 +1,16 @@
+package com.deka.sate.data;
+
+public class Sate {
+
+  public static String[] sateMenu = {
+      "SATE KAMBING",
+      "SATE AYAM",
+      "SATE LILIT"
+  };
+
+  public static String[] sateDetails = {
+      "Daging kambing yang disate dan dibumbui dengan rempah-rempah dan bumbu dapur, kemudian dibakar.",
+      "Daging ayam yang dipotong kecil-kecil dan ditusuk sedemikian rupa dengan tusukan lidi tulang daun kelapa atau bambu, kemudian dipanggang menggunakan bara arang kayu.",
+      "Varian sate asal Bali yang terbuat dari daging babi, ikan, ayam, daging sapi, atau bahkan kura-kura yang dicincang, kemudian dicampur dengan parutan kelapa, santan, jeruk nipis, bawang merah, dan merica."
+  };
+}
